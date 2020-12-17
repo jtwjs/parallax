f71003f73971bb8054238c4a42e534c046449092
Jung taeWoong <60641307+jtwjs@users.noreply.github.com>
2020-12-17 15:39:45 +0900

    JS: 1. add scrollHandler function, 2. function name change

--- Parallax/001/main.js
+++ Parallax/001/main.js
@@ -41,7 +41,7 @@
         }
     }
 
-    function scrollMenuActive() {
+    function scrollSectionObserve() {
         const sectionList = Array.from(main.children);
         const observerOption = {
             root: null,
@@ -51,15 +51,7 @@
         const observerCallback = (entries, observer) => {
             entries.forEach((entry) => {
                 if(!entry.isIntersecting && entry.intersectionRatio > 0) {
-                    const index = sectionList.map(x => x.id).indexOf(`${entry.target.id}`);
-                    let selectedIndex;
-                    if (entry.boundingClientRect.y < 0) {
-                        selectedIndex = index + 1;
-                    } else {
-                        selectedIndex = index - 1;
-                    }
-                    const navItem = document.querySelector(`[data-category="#${sectionList[selectedIndex].id}"]`);
-                    oneOfThem(navItem);
+                    scrollMenuActive(entry);
                 } 
             });
         }
@@ -68,7 +60,20 @@
             observer.observe(section);
         }
     }
-    scrollMenuActive();
+    scrollSectionObserve();
+
+    function scrollMenuActive(entry) {
+        const sectionList = Array.from(main.children);
+        const index = sectionList.map(x => x.id).indexOf(`${entry.target.id}`);
+        let selectedIndex;
+        if (entry.boundingClientRect.y < 0) {
+            selectedIndex = index + 1;
+        } else {
+            selectedIndex = index - 1;
+        }
+        const navItem = document.querySelector(`[data-category="#${sectionList[selectedIndex].id}"]`);
+        oneOfThem(navItem);
+    }
 
     function scrollIntoView(elem){ 
         const scrollTo = document.querySelector(elem);
@@ -76,7 +81,20 @@
         window.scrollTo({top: topValue, behavior: 'smooth'});
     }
 
+    function scrollHandler() {
+        const yOffset = window.pageYOffset;
+        const height = main.children[0].clientHeight;
+        const stickyLocationY = height - header.clientHeight;
+        if(yOffset > stickyLocationY) {
+            header.classList.add('sticky');
+        } else {
+            header.classList.remove('sticky');
+        }
+
+    }
+
     function init() {
+        window.addEventListener('scroll', scrollHandler);
         header.addEventListener('click',headerClickHandler);   
     }
     init();
